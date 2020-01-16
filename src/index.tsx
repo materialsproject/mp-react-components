bc00d3ec7e755fd5c847d070da56bab0502d1873
Francois Chabbey <francois@streamsets.com>
2020-01-16 14:44:06 -0800

    Fix compilation error. Remove NL

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,13 @@
  *  App bootstrapping. See index.html
  *
  */
-
-
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
-
-//import { Table } from './periodic-table/periodic-table-component/periodic-table.component';
 import "./styles.less";
-import { WithSelection } from "./periodic-table/table-state";
+import { SelectableTable } from "./periodic-table/table-state";
 
 const mountNodeSelector = 'app';
 const mountNode = document.getElementById(mountNodeSelector);
 
-
-/*ReactDOM.render(<Table onElementClicked={(e) => console.log(e)} enabledElement={enabledElement}
-                       disabledElement={disabledElement}/>, mountNode);*/
-
-ReactDOM.render(<WithSelection enabledElements={{'H': true, 'C': true}} disabledElements={{'Na':true, 'Ca':true}} />, mountNode);
+ReactDOM.render(<SelectableTable enabledElements={{'H': true, 'C': true}} disabledElements={{'Na':true, 'Ca':true}} />, mountNode);
 console.log('RUNNING in',  process.env.NODE_ENV, 'DEBUGGING IS', process.env.DEBUG);
