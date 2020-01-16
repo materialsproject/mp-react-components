cb49accd518f60a9ec9cf20876fc2ad46f9adbfb
Francois Chabbey <francois@streamsets.com>
2020-01-16 12:47:27 -0800

    Add support for multiple environment

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import * as ReactDOM from "react-dom";
 
 import { Table } from './periodic-table/periodic-table-component/periodic-table.component';
 import "./styles.less";
+import { WithSelection } from "./periodic-table/table-state";
 
 const mountNodeSelector = 'app';
 const mountNode = document.getElementById(mountNodeSelector);
@@ -17,5 +18,8 @@ const mountNode = document.getElementById(mountNodeSelector);
 const enabledElement: any = {Be:true, Na:true, Cl:true};
 const disabledElement: any = {Li:true, He:true};
 
-ReactDOM.render(<Table onElementClicked={(e) => console.log(e)} enabledElement={enabledElement}
-                       disabledElement={disabledElement}/>, mountNode);
+/*ReactDOM.render(<Table onElementClicked={(e) => console.log(e)} enabledElement={enabledElement}
+                       disabledElement={disabledElement}/>, mountNode);*/
+
+ReactDOM.render(<WithSelection/>, mountNode);
+console.log('RUNNING in', process.env.DEBUG, process.env.NODE_ENV);
