e5359cecd33acea670c53966e7de067c7bd4cca9
Francois Chabbey <francois@streamsets.com>
2020-01-17 12:05:07 -0800

    Add enzyme.Setup enzyme. Add component test
    - Jest is not able to handle css modules. We mock them by an empty object

--- src/periodic-table/periodic-element/periodic-element.spec.tsx
+++ src/periodic-table/periodic-element/periodic-element.spec.tsx
@@ -5,19 +5,42 @@ import { PeriodicElement } from "./periodic-element.component";
 //JEST does not know how to handle LESS
 jest.mock('./periodic-element.module.less', () => {});
 
-describe('<PeriodicElement/>', () => {
-  const wrapper = shallow(<PeriodicElement
-    onElementClicked={()=>{}}
-    disabled={true}
-    enabled={false}
-    element={{class:'a', symbol:'H', elementNumber:'12'}} />);
+fdescribe('<PeriodicElement/>', () => {
 
-
-  fit('should have correct class', () => {
-    expect(wrapper.find('.mat-element').hasClass('m')).toBe(false);
+  it('should be rendered', () => {
+    const wrapper = renderElement();
+    expect(wrapper.find('.mat-element').length).toBe(1);
+    expect(wrapper.find('.mat-element').hasClass('disabled')).toBe(false);
+    expect(wrapper.find('.mat-element').hasClass('enabled')).toBe(false);
   });
-  it('should have correct enabled/disabled state', () => {
-
+  it('should have correct enabled/disabled state / 1', () => {
+    const wrapper = renderElement(true, true);
+    expect(wrapper.find('.mat-element').hasClass('disabled')).toBe(true);
+    expect(wrapper.find('.mat-element').hasClass('enabled')).toBe(false);
   });
-
+  it('should have correct enabled/disabled state / 2', () => {
+    const wrapper = renderElement(true, true);
+    expect(wrapper.find('.mat-element').hasClass('disabled')).toBe(true);
+    expect(wrapper.find('.mat-element').hasClass('enabled')).toBe(false);
+  });
+  it('should have correct enabled/disabled state / 3', () => {
+    const wrapper = renderElement(true, false);
+    expect(wrapper.find('.mat-element').hasClass('disabled')).toBe(true);
+    expect(wrapper.find('.mat-element').hasClass('enabled')).toBe(false);
+  });
+  it('click callback shoud be called', ()=> {
+    const mockCallBack = jest.fn();
+    const wrapper = renderElement(true, false, mockCallBack);
+    wrapper.find('.mat-element').simulate('click');
+    expect(mockCallBack).toHaveBeenCalledTimes(1);
+  })
 });
+
+
+function renderElement(disabled = false,
+                       enabled = false,
+                       onClick = () => {},
+                       element = {class:'a', symbol:'H', elementNumber:'12'},
+                      ) {
+  return shallow(<PeriodicElement disabled={disabled} enabled={enabled} element={element} onElementClicked={onClick}/>)
+}
