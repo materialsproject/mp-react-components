2c648363fcf8d32cf52dac9d260b24192125cc63
Francois Chabbey <francois@streamsets.com>
2020-01-27 10:13:53 -0800

    Exposes periodic element component:
    People might want to display a component itself

--- src/periodic-table/index.ts
+++ src/periodic-table/index.ts
@@ -1,7 +1,11 @@
 import { SelectableTable } from "./table-state";
 import { TableFilter } from "./periodic-filter/table-filter";
+import { PeriodicElement } from "./periodic-element/periodic-element.component";
 
 export {
   SelectableTable,
-  TableFilter
+  TableFilter,
+  PeriodicElement
 }
+
+
