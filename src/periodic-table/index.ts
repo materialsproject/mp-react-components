2e7567301cad7d6a786f6a824fe36598e307ef9c
Francois Chabbey <francois@streamsets.com>
2020-02-07 09:19:28 -0800

    Expose the crystal toolkit components

--- src/periodic-table/index.ts
+++ src/periodic-table/index.ts
@@ -2,12 +2,21 @@ import { SelectableTable } from "./table-state";
 import { TableFilter } from "./periodic-filter/table-filter";
 import { StandalonePeriodicComponent } from "./periodic-element/standalone-periodic-component";
 import { PeriodicContext} from "./periodic-table-state/periodic-selection-context";
+//TODO(chab) reorganize export, use a barrel for each folder ( ? ), and have a general index at root-level
+import Simple3DSceneComponent
+  from "../crystal-toolkit-components/components-v2/Simple3DScene/Simple3DSceneComponent.react";
+import { JSONViewComponent } from "../crystal-toolkit-components/components-v2/JSONViewComponent.react";
+import GraphComponent from "../crystal-toolkit-components/components-v2/graph.component";
+
 
 export {
   SelectableTable,
   TableFilter,
   StandalonePeriodicComponent,
-  PeriodicContext
+  PeriodicContext,
+  Simple3DSceneComponent,
+  JSONViewComponent,
+  GraphComponent
 }
 
 
