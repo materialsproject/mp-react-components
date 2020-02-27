2776c286ac96fedfed6204ad20b2c0f29e71c780
Francois Chabbey <francois@streamsets.com>
2020-02-26 17:14:10 -0800

    Move JSONView to functional component

--- src/crystal-toolkit-components/components-v2/JSONViewComponent.react.tsx
+++ src/crystal-toolkit-components/components-v2/JSONViewComponent.react.tsx
@@ -1,108 +1,85 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes, { InferProps } from 'prop-types';
 import ReactJson from 'react-json-view';
 
-/**
- * JSONViewComponent renders JSON using
- * react-json-view from @mac-s-g
- */
-export class JSONViewComponent extends Component<
-  InferProps<typeof JSONViewComponent.propTypes>,
-  any
-> {
-  static propTypes = {
-    /**
-     * The ID used to identify this component in Dash callbacks
-     */
-    id: PropTypes.string,
-    type: PropTypes.oneOf(['array', 'object']),
-    // see documentation at https://github.com/mac-s-g/react-json-view
-    src: PropTypes.object,
-    name: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-    theme: PropTypes.string,
-    style: PropTypes.object,
-    iconStyle: PropTypes.oneOf(['circle', 'triangle', 'square']),
-    indentWidth: PropTypes.number,
-    collapsed: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
-    collapseStringsAfterLength: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
-    groupArraysAfterLength: PropTypes.number,
-    enableClipboard: PropTypes.bool,
-    displayObjectSize: PropTypes.bool,
-    displayDataTypes: PropTypes.bool,
-    defaultValue: PropTypes.object,
-    sortKeys: PropTypes.bool,
-    validationMessage: PropTypes.string,
+function JSONViewComponent(props: InferProps<typeof JSONViewComponent.propTypes>) {
+  const {
+    src,
+    name,
+    theme,
+    style,
+    iconStyle,
+    indentWidth,
+    collapsed,
+    collapseStringsAfterLength,
+    groupArraysAfterLength,
+    enableClipboard,
+    displayObjectSize,
+    displayDataTypes,
+    defaultValue,
+    sortKeys,
+    validationMessage
+  } = props as any;
 
-    /**
-     * Dash-assigned callback that should be called whenever any of the
-     * properties change
-     */
-    setProps: PropTypes.func
-  };
-
-  // https://github.com/mac-s-g/react-json-view/blob/39f7b6b2e73a873b974f0801c260d90d76cccdae/index.d.ts
-  static defaultProps = {
-    src: null,
-    name: false,
-    theme: 'rjv-default',
-    style: {},
-    iconStyle: 'circle',
-    indentWidth: 8,
-    collapsed: false,
-    collapseStringsAfterLength: false,
-    groupArraysAfterLength: 100,
-    enableClipboard: true,
-    displayObjectSize: false,
-    displayDataTypes: false,
-    defaultValue: null,
-    sortKeys: false,
-    validationMessage: 'Validation Error'
-  };
-
-  constructor(public props: any) {
-    super(props);
-  }
-
-  render() {
-    const {
-      src,
-      name,
-      theme,
-      style,
-      iconStyle,
-      indentWidth,
-      collapsed,
-      collapseStringsAfterLength,
-      groupArraysAfterLength,
-      enableClipboard,
-      displayObjectSize,
-      displayDataTypes,
-      defaultValue,
-      sortKeys,
-      validationMessage
-    } = this.props as any;
-
-    return (
-      <ReactJson
-        src={src}
-        name={name}
-        theme={theme}
-        style={style}
-        iconStyle={iconStyle}
-        indentWidth={indentWidth}
-        collapsed={collapsed}
-        collapseStringsAfterLength={collapseStringsAfterLength}
-        groupArraysAfterLength={groupArraysAfterLength}
-        enableClipboard={enableClipboard}
-        displayObjectSize={displayObjectSize}
-        displayDataTypes={displayDataTypes}
-        defaultValue={defaultValue}
-        sortKeys={sortKeys}
-        validationMessage={validationMessage}
-        onEdit={e => {}}
-        onAdd={a => {}}
-        onDelete={d => {}}
-      />
-    );
-  }
+  return (
+    <ReactJson
+      src={src}
+      name={name}
+      theme={theme}
+      style={style}
+      iconStyle={iconStyle}
+      indentWidth={indentWidth}
+      collapsed={collapsed}
+      collapseStringsAfterLength={collapseStringsAfterLength}
+      groupArraysAfterLength={groupArraysAfterLength}
+      enableClipboard={enableClipboard}
+      displayObjectSize={displayObjectSize}
+      displayDataTypes={displayDataTypes}
+      defaultValue={defaultValue}
+      sortKeys={sortKeys}
+      validationMessage={validationMessage}
+      onEdit={e => {}}
+      onAdd={a => {}}
+      onDelete={d => {}}
+    />
+  );
 }
+
+JSONViewComponent.propTypes = {
+  type: PropTypes.oneOf(['array', 'object']),
+  // see documentation at https://github.com/mac-s-g/react-json-view
+  src: PropTypes.object,
+  name: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  theme: PropTypes.string,
+  style: PropTypes.object,
+  iconStyle: PropTypes.oneOf(['circle', 'triangle', 'square']),
+  indentWidth: PropTypes.number,
+  collapsed: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  collapseStringsAfterLength: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  groupArraysAfterLength: PropTypes.number,
+  enableClipboard: PropTypes.bool,
+  displayObjectSize: PropTypes.bool,
+  displayDataTypes: PropTypes.bool,
+  defaultValue: PropTypes.object,
+  sortKeys: PropTypes.bool,
+  validationMessage: PropTypes.string
+};
+
+// https://github.com/mac-s-g/react-json-view/blob/39f7b6b2e73a873b974f0801c260d90d76cccdae/index.d.ts
+JSONViewComponent.defaultProps = {
+  src: null,
+  name: false,
+  theme: 'rjv-default',
+  style: {},
+  iconStyle: 'circle',
+  indentWidth: 8,
+  collapsed: false,
+  collapseStringsAfterLength: false,
+  groupArraysAfterLength: 100,
+  enableClipboard: true,
+  displayObjectSize: false,
+  displayDataTypes: false,
+  defaultValue: null,
+  sortKeys: false,
+  validationMessage: 'Validation Error'
+};
