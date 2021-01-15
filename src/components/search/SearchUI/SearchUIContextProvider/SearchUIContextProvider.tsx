c57f6254168cda42b1ab2375ab16466a4705631f
Cody O'Donnell <ctodonnell@lbl.gov>
2021-01-15 16:45:27 -0500

    Changed hook function declarations to fix strange type error with useQuery hook

--- src/components/search/SearchUI/SearchUIContextProvider/SearchUIContextProvider.tsx
+++ src/components/search/SearchUI/SearchUIContextProvider/SearchUIContextProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import qs from 'qs';
-import { useDeepCompareDebounce, usePrevious, useQuery } from '../../../../utils/hooks';
+import { usePrevious, useQuery } from '../../../../utils/hooks';
 import {
   FilterGroup,
   FilterType,
