0f53b946180d906fa2c12c4030701cc88643fec4
Cody O'Donnell <ctodonnell@lbl.gov>
2021-05-05 15:03:11 -0700

    Fix mock data for SearchUI test

--- src/mocks/constants/materialsById.ts
+++ src/mocks/constants/materialsById.ts
@@ -2,7 +2,7 @@ export const materialsByIdQuery = 'mp-24230';
 export const materialsByIdResponse = {
   data: [
     {
-      task_id: 'mp-24230',
+      material_id: 'mp-24230',
       nsites: 24,
       formula_pretty: 'HCNO',
       volume: 266.90756394969907,
@@ -13,13 +13,13 @@ export const materialsByIdResponse = {
         number: 15,
       },
       formation_energy_per_atom: -0.9689243143125003,
-      e_above_hull: 0,
+      energy_above_hull: 0,
       is_stable: true,
     },
   ],
   meta: {
     max_limit: 100,
-    default_fields: ['task_id'],
+    default_fields: ['material_id'],
     total: 1,
   },
 };
