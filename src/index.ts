fd56e994928f42a62df1d5703a160c607ee56542
Cody O'Donnell <ctodonnell@lbl.gov>
2021-05-10 12:28:55 -0700

    Export new bibjson components

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,22 @@ import { SelectableTable } from './components/periodic-table/table-state';
 import { TableFilter } from './components/periodic-table/periodic-filter/table-filter';
 import { StandalonePeriodicComponent } from './components/periodic-table/periodic-element/standalone-periodic-component';
 import { PeriodicContext } from './components/periodic-table/periodic-table-state/periodic-selection-context';
-// import SearchFunnel from './components/search/exportable-grid';
 import { CrystalToolkitScene } from './components/crystal-toolkit/CrystalToolkitScene/CrystalToolkitScene';
 import { CameraContextProvider } from './components/crystal-toolkit/CameraContextProvider';
 import JSONViewComponent from './components/crystal-toolkit/JSONViewComponent.react';
 import ReactGraphComponent from './components/crystal-toolkit/graph.component';
 import { Sidebar } from './components/navigation/Sidebar';
-// import { GridWithContext } from './components/search/search-grid/card-grid';
-// import { MtPrintViewContext, MTGrid, MtMaterialTable } from './components/search/exportable-grid-v2';
 import Scene from './components/crystal-toolkit/scene/Scene';
 import { Scrollspy } from './components/navigation/Scrollspy';
 import { MaterialsInput } from './components/search/MaterialsInput/MaterialsInput';
 import { SearchUI } from './components/search/SearchUI';
 import DataTable from 'react-data-table-component';
 import { GlobalSearchBar } from './components/search/GlobalSearchBar';
-import {
-  Wrapper as MenuWrapper,
-  Button as MenuButton,
-  Menu,
-  MenuItem,
-} from 'react-aria-menubutton';
 import { NavbarDropdown } from './components/navigation/NavbarDropdown';
 import { Select } from './components/search/Select';
 import { Download } from './components/crystal-toolkit/Download';
+import { BibjsonFilter } from './components/search/BibjsonFilter';
+import { BibjsonCard } from './components/search/BibjsonCard';
 
 export {
   SelectableTable,
@@ -47,4 +40,6 @@ export {
   NavbarDropdown,
   Select,
   Download,
+  BibjsonFilter,
+  BibjsonCard,
 };
--- src/views/Publications/Publications.tsx
+++ src/views/Publications/Publications.tsx
@@ -48,6 +48,7 @@ export const Publications: React.FC = () => {
         },
       ],
     },
+    {},
   ];
 
   return (
