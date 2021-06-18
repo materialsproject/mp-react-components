90eb047ef7b3eb439437a3530d0c487041750498
Cody O'Donnell <ctodonnell@lbl.gov>
2021-06-17 17:26:05 -0700

    Format formulas in active filter buttons

--- src/components/search/ActiveFilterButtons/ActiveFilterButtons.tsx
+++ src/components/search/ActiveFilterButtons/ActiveFilterButtons.tsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
 import { Button } from 'react-bulma-components';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaTimesCircle } from 'react-icons/fa';
+import { Formula } from '../Formula';
+import { validateFormula } from '../MaterialsInput/utils';
 import { ActiveFilter } from '../SearchUI/types';
 import { formatPointGroup } from '../utils';
 import './ActiveFilterButtons.css';
@@ -17,6 +19,8 @@ const formatValue = (filter: ActiveFilter) => {
     return filter.value[0] + ' to ' + filter.value[1];
   } else if (filter.id === 'pointgroup') {
     return formatPointGroup(filter.value);
+  } else if (validateFormula(filter.value.toString())) {
+    return <Formula>{filter.value.toString()}</Formula>;
   } else {
     return filter.value.toString();
   }
@@ -27,15 +31,12 @@ export const ActiveFilterButtons: React.FC<Props> = (props) => {
     <div data-testid="active-filter-buttons" className="mpc-active-filter-buttons">
       {props.filters.map((f, i) => (
         <div className="mpc-active-filter-button" key={i}>
-          <Button
-            className="is-rounded is-small"
-            onClick={() => props.onClick(f.defaultValue, f.id)}
-          >
-            <FaTimes />
+          <button className="button is-small" onClick={() => props.onClick(f.defaultValue, f.id)}>
+            <FaTimesCircle />
             <span className="ml-1">
               {f.displayName}: {formatValue(f)}
             </span>
-          </Button>
+          </button>
         </div>
       ))}
     </div>
