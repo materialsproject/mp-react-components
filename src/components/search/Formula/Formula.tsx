74d56afeffb1398b37e24bdf61f3a822907298c6
Cody O'Donnell <ctodonnell@lbl.gov>
2021-07-27 09:36:56 -0700

    Modified formula component to use elements regex and subscript non-elements. Modified synthesis card styles on small screens.

--- src/components/search/Formula/Formula.tsx
+++ src/components/search/Formula/Formula.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
+import { ELEMENTS_REGEX, ELEMENTS_SPLIT_REGEX } from '../MaterialsInput/utils';
 
 interface Props {
   id?: string;
@@ -9,7 +10,7 @@ interface Props {
 }
 
 const formulaItem = (str: string) => {
-  if (parseFloat(str) || str === '-' || str === 'x') {
+  if (!str.match(/\(|\)/g) && !str.match(ELEMENTS_REGEX)) {
     return <sub>{str}</sub>;
   } else {
     return <span>{str}</span>;
@@ -17,21 +18,15 @@ const formulaItem = (str: string) => {
 };
 
 export const Formula: React.FC<Props> = (props) => {
-  const hasSubscripts = props.children.match(/\d|x/) !== null;
   let formula: React.ReactNode;
-  if (hasSubscripts) {
-    const splitFormula: string[] = props.children.split(/(\d*\.?\d+|\-|x)/g);
-    const formulaParts = splitFormula.filter((d) => d !== '' && d !== '1');
-    formula = (
-      <span>
-        {formulaParts.map((s, i) => (
-          <span key={i}>{formulaItem(s)}</span>
-        ))}
-      </span>
-    );
-  } else {
-    formula = props.children;
-  }
+  const splitFormula = props.children.match(ELEMENTS_SPLIT_REGEX);
+  formula = (
+    <span>
+      {splitFormula?.map((s, i) => (
+        <span key={i}>{formulaItem(s)}</span>
+      ))}
+    </span>
+  );
 
   return (
     <span id={props.id} className={classNames('mpc-formula', props.className)}>
