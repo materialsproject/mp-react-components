c14429ac676b09bb0b15bbc9545f76eef8333c7b
Min-Hsueh Chiu <peter810601@gmail.com>
2026-03-09 18:00:09 -0700

    upgrade parcel

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import '../node_modules/bulma/css/bulma.min.css';
 import '../src/styles.less';
-import '../src/assets/fonts.css';
+// import '../src/assets/fonts.css';
 import '../src//stories/stories.css';
 
 export const parameters = {
